e42d71303890b91af7e9119106e23fdfccae5d5b
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-10 16:15:35 +0300

    created users tables

--- app/models.py
+++ app/models.py
@@ -3,16 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class UserFavs(db.Model):
-	__tablename__ = 'users'
-	username = db.Column(db.String, primary_key=True)
-	place = db.Column(db.String)
-	food = db.Column(db.String)
+    __tablename__ = 'users'
 
-	def __init__(self, username, place, food):
-		self.username=username
-		self.place=place
-		self.food=food
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
 
-	def __repr__(self):
-		return f'<User-Place-Food : {self.username}-{self.place}-{self.food}'
+    def __init__(self,name):
+        self.name = name        
 
+    def json(self):
+        return {'name': self.name}
+
+    @classmethod
+    def find_user_by_name(cls,name): 
+       return cls.query.filter_by(name=name).first_or_404()
